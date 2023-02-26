e2e920f1951ef5811e10104c4cc63cafdf4ab374
ParkBmo <gh repo clone kh-academy-java/java-class-a8>
2023-02-27 03:05:16 +0900

    스프링파일로 변경

---
+++ ParkBMO/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package main.java.com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
