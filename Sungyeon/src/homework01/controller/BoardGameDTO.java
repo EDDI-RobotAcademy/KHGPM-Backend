1efcdd77d3decedb4dc897594906d7141b0ea3da
Castleyear <ksy48562@gmail.com>
2023-02-26 19:45:58 +0900

    Sungyeon Backend 2 과제제출

---
+++ Sungyeon/src/homework01/controller/BoardGameDTO.java
@@ -0,0 +1,16 @@
+package homework01.controller;
+import lombok.extern.slf4j.Slf4j;
+import lombok.Getter;
+import lombok.ToString;
+
+
+
+@ToString
+@Getter
+public class BoardGameDTO {
+
+    private String winner;
+
+}
+
+
