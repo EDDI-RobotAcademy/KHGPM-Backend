a846dd055ab518eb5aabf959a97ad15f5fc8aaf6
AnnSungHee <d980104@gmail.com>
2023-03-03 17:56:23 +0900

    add : [안성희] "주석처리중"

--- AnnSungHee/BoardCreationTest/dicegame/src/utility/CustomRandom.java
+++ AnnSungHee/BoardCreationTest/dicegame/src/utility/CustomRandom.java
@@ -9,6 +9,12 @@ public class CustomRandom {
 
     private static Random random = new Random();
 
+    /**
+     * createCustomRandom 함수 선언
+     * @param min 최솟값
+     * @param max 최대값
+     * @return
+     */
     public static int createCustomRandom (int min, int max) {
         return random.nextInt(max - min + 1) + min;
     }
