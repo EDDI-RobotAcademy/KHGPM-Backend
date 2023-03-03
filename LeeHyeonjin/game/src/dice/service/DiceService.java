26b01f6d5b9749e8b3fc9395a7fe91fb74bdf123
hjlee5701 <hjlee5701@gmail.com>
2023-03-03 21:48:06 +0900

    0303 연습

--- LeeHyeonjin/game/src/dice/service/DiceService.java
+++ LeeHyeonjin/game/src/dice/service/DiceService.java
@@ -1,5 +1,6 @@
 package dice.service;
 
+
 import dice.Dice;
 
 public class DiceService {
@@ -7,9 +8,15 @@ public class DiceService {
     final private int MIN = 1;
     final private int MAX = 6;
 
-    final private int generalDice;
-    final private Dice specialDiceNumber;
+    final private Dice generalDice;
 
+
+    public DiceService(Dice generalDice, Dice specialDice) {
+        this.generalDice = generalDice;
+    }
+    public int getGeneralDice() {
+        return this.generalDice.getDiceNumber();
+    }
 
 
 
