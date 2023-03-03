777f2144fdc17f2430cc9735615b2720671a9c6e
hjlee5701 <hjlee5701@gmail.com>
2023-03-03 17:24:38 +0900

    feat: 커스텀 랜덤 유틸리티 [DICE-GAME-2]

---
+++ LeeHyeonjin/game/src/dice/service/DiceService.java
@@ -0,0 +1,18 @@
+package dice.service;
+
+import dice.Dice;
+
+public class DiceService {
+
+    final private int MIN = 1;
+    final private int MAX = 6;
+
+    final private int generalDice;
+    final private Dice specialDiceNumber;
+
+
+
+
+
+
+}
--- LeeHyeonjin/game/src/utility/CustomRandom.java
+++ LeeHyeonjin/game/src/utility/CustomRandom.java
@@ -3,12 +3,12 @@ package utility;
 import java.util.Random;
 
 public class CustomRandom {
-    private static int max=6;
-    private static int min=1;
+    private static int max;
+    private static int min;
 
     private static Random random = new Random();
 
-    public static int customRandom(int max, int min){
+    public static int createcustomRandom(int max, int min){
         return random.nextInt(max-min+1) + min;
     }
 
