60d97bce1687466b97ac5cf33ed07ebbad54b3f7
dduru-nuna <syj00062@gmail.com>
2023-03-04 13:38:10 +0900

    comment: [서윤지] 어노테이션 및 메서드 설명 주석 추가

--- SeoYoonji/spring/demo/src/main/java/com/example/demo/domain/board/controller/BoardController.java
+++ SeoYoonji/spring/demo/src/main/java/com/example/demo/domain/board/controller/BoardController.java
@@ -5,9 +5,9 @@ import com.example.demo.domain.board.service.BoardService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-@Slf4j
-@RestController
-@RequestMapping("/board")
+@Slf4j //로그 남기기
+@RestController  //@Controller + @ResponseBody -> 단순히 객체만을 반환. JSON 형식으로 객체 데이터를 전송한다.
+@RequestMapping("/board") //클라이언트 요청 처리 url 맵핑
 @CrossOrigin(origins = "http://localhost:8080", allowedHeaders = "*")
 public class BoardController {
 
@@ -19,6 +19,7 @@ public class BoardController {
 
     @PostMapping("/register")
     public void boardRegister (@RequestBody BoardRequest boardRequest) {
+        //@RequestBody 가 붙은 파라미터에는 http요청의 본문 body가 그대로 전달됨
         log.info("boardRegister()");
 
         boardService.register(boardRequest);
--- SeoYoonji/spring/demo/src/main/java/com/example/demo/domain/board/service/BoardServiceImpl.java
+++ SeoYoonji/spring/demo/src/main/java/com/example/demo/domain/board/service/BoardServiceImpl.java
@@ -19,10 +19,12 @@ public class BoardServiceImpl implements BoardService {
     @Override
     public void register(BoardRequest boardRequest) {
         Board board = new Board();
+        // 요청받은 데이터 값으로 set
         board.setTitle(boardRequest.getTitle());
         board.setWriter(boardRequest.getWriter());
         board.setContent(boardRequest.getContent());
 
         boardRepository.save(board);
+        // JpaRepository를 상속받으면 기본적으로 제공하는 메서드 중 하나인 save
     }
 }
