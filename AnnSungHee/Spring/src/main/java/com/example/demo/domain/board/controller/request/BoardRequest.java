25ee9f3af005e6e51ee102f435ed5efcefffa88e
AnnSungHee <d980104@gmail.com>
2023-03-06 21:37:48 +0900

    feat: [안성희] Board에서 삭제 수정 조회 기능 추가

--- AnnSungHee/Spring/src/main/java/com/example/demo/domain/board/controller/request/BoardRequest.java
+++ AnnSungHee/Spring/src/main/java/com/example/demo/domain/board/controller/request/BoardRequest.java
@@ -1,8 +1,13 @@
 package com.example.demo.domain.board.controller.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class BoardRequest {
 
     private String title;
