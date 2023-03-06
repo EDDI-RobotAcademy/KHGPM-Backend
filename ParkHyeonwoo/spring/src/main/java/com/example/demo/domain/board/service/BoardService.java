8af6ed24dc3c2067f3bbeddbebc8a24bcd761f9c
Hyeonvv <pphhww5017@gmail.com>
2023-03-06 16:48:50 +0900

    feat: 게시물 리스트 보기

--- ParkHyeonwoo/spring/src/main/java/com/example/demo/domain/board/service/BoardService.java
+++ ParkHyeonwoo/spring/src/main/java/com/example/demo/domain/board/service/BoardService.java
@@ -1,7 +1,12 @@
 package com.example.demo.domain.board.service;
 
 import com.example.demo.domain.board.controller.request.BoardRequest;
+import com.example.demo.domain.board.entity.Board;
+
+import java.util.List;
 
 public interface BoardService {
     public void register(BoardRequest boardRequest);
+
+    List<Board> list();
 }
