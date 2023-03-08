38840539f0752c4093edc7c4d05a63a832d9114b
hjlee5701 <hjlee5701@gmail.com>
2023-03-08 20:56:52 +0900

    [spring] 게시판

--- LeeHyeonjin/spring/src/main/java/com/example/demo/domain/board/controller/BoardController.java
+++ LeeHyeonjin/spring/src/main/java/com/example/demo/domain/board/controller/BoardController.java
@@ -1,9 +1,12 @@
 package com.example.demo.domain.board.controller;
 
 import com.example.demo.domain.board.controller.request.BoardRequest;
+import com.example.demo.domain.board.entity.Board;
 import com.example.demo.domain.board.service.BoardService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -20,7 +23,33 @@ public class BoardController {
     @PostMapping("/register")
     public void boardRegister (@RequestBody BoardRequest boardRequest) {
         log.info("boardRegister()");
-
+        // 요청 처리함, 어떤 요청이 들어왔는지 알 수 있음 , 받이오기만하기 때문에 getter만 함
         boardService.register(boardRequest);
     }
+    @GetMapping("/list")
+    public List<Board> boardList () {
+        log.info("boardList()");
+
+        return boardService.list();
+    }
+
+    @GetMapping("/{boardId}")
+    public Board boardRead(@PathVariable("boardId") Long boardId) {
+        log.info("boardRead()");
+
+        return boardService.read(boardId);
+    }
+
+    @DeleteMapping("/{boardId}")
+    public void boardRemove(@PathVariable("boardId") Long boardId){
+        log.info("boardRemove()");
+        boardService.remove(boardId);
+    }
+
+    @PutMapping("/{boardId}")
+    public void boardModify(@PathVariable("boardId") Long boardId,
+                            @RequestBody BoardRequest boardRequest){
+        boardService.modify(boardId, boardRequest);
+    }
+
 }
--- LeeHyeonjin/spring/src/main/java/com/example/demo/domain/board/controller/request/BoardRequest.java
+++ LeeHyeonjin/spring/src/main/java/com/example/demo/domain/board/controller/request/BoardRequest.java
@@ -1,11 +1,15 @@
 package com.example.demo.domain.board.controller.request;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
 
 @Getter
+@NoArgsConstructor
 public class BoardRequest {
 
-    private String title;
-    private String writer;
-    private String content;
+    private  String title;
+    private  String writer;
+    private  String content;
 }
