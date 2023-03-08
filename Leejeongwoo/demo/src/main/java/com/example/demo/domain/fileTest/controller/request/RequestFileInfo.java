8129a4e59f26bbe9bdeea61c81b323c1ffc757b2
jeongwoogithub <leejeongpop@gmail.com>
2023-03-08 18:22:22 +0900

    feat:[이정우]이미지파일 업로드 기능 추가

---
+++ Leejeongwoo/demo/src/main/java/com/example/demo/domain/fileTest/controller/request/RequestFileInfo.java
@@ -0,0 +1,12 @@
+package com.example.demo.domain.fileTest.controller.request;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class RequestFileInfo {
+
+    final private Integer price;
+    final private String test;
+}
