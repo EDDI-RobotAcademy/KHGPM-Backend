4b6f149ed141f2a0eaa4920051bb41b38f3605dc
gomgomehoya <gomgomehoya@gmail.com>
2023-03-09 21:14:20 +0900

    add: [김호유] 03.09 수업내용 커밋 테스트

---
+++ Kimhoyou/demo/main/src/main/java/com/example/demo/domain/vueTest/controller/request/VueRequestTestData.java
@@ -0,0 +1,13 @@
+package com.example.demo.domain.vueTest.controller.request;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+public class VueRequestTestData {
+
+    private String memberName;
+    private String major;
+    private String receivedWinner;
+}
