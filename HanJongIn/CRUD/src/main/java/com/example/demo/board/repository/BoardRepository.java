75468f422890d1d3de07b05b1f6601dad37f16f7
jonginhan <instantgogo@gmail.com>
2023-03-10 01:36:49 +0900

    fix: BoardRepository 제네릭 추가 [CRUD 2]

--- HanJongIn/CRUD/src/main/java/com/example/demo/board/repository/BoardRepository.java
+++ HanJongIn/CRUD/src/main/java/com/example/demo/board/repository/BoardRepository.java
@@ -1,9 +1,10 @@
 package com.example.demo.board.repository;
 
+import com.example.demo.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BoardRepository extends JpaRepository {
+public interface BoardRepository extends JpaRepository<Board, Integer> {
 
 }
