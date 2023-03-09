d3d453c44337ce2b0bdc6496fb473858028589ed
taylor930422 <125858598+taylor930422@users.noreply.github.com>
2023-03-09 21:04:36 +0900

    feat: [박태현] 파일 업로드 기능 추가

---
+++ ParkTaeHyeon/demo/src/main/java/com/example/demo/domain/fileTest/controller/request/RequestFileInfo.java
@@ -0,0 +1,14 @@
+package com.example.demo.domain.fileTest.controller.request;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class RequestFileInfo {
+
+    final private Integer price;
+
+    final private String test;
+
+}
