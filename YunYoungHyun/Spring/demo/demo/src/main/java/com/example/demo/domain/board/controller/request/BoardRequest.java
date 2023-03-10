a8a2848ee53eda6ae4529582ca016389b621180a
vceqiv09 <vceqiv@gmail.com>
2023-03-10 16:13:28 +0900

    feat:[윤영현] BoardRequest 에 생성자 추가

--- YunYoungHyun/Spring/demo/demo/src/main/java/com/example/demo/domain/board/controller/request/BoardRequest.java
+++ YunYoungHyun/Spring/demo/demo/src/main/java/com/example/demo/domain/board/controller/request/BoardRequest.java
@@ -1,12 +1,16 @@
 package com.example.demo.domain.board.controller.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@AllArgsConstructor
 public class BoardRequest {
 
     private String title;
     private String writer;
     private String content;
+
 
 }
