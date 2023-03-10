e4fcf89f1c3181162bcea7c337f43e4f58230b43
dduru-nuna <syj00062@gmail.com>
2023-03-10 16:14:09 +0900

    feat: [서윤지] 게시판 테스트

---
+++ SeoYoonji/spring/demo/src/test/java/com/example/demo/board/BoardTests.java
@@ -0,0 +1,41 @@
+package com.example.demo.board;
+
+import com.example.demo.domain.board.controller.request.BoardRequest;
+import com.example.demo.domain.board.entity.Board;
+import com.example.demo.domain.board.service.BoardService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BoardTests {
+    @Autowired
+    private BoardService boardService;
+    @Test
+    public void 게시물_저장_테스트() {
+        BoardRequest boardRequest = new BoardRequest("test", "test", "test");
+        boardService.register(boardRequest);
+    }
+
+    @Test
+    public void 게시물_리스트_테스트() {
+        System.out.println(boardService.list());
+    }
+
+    @Test
+    public void 게시물_읽기_테스트() {
+       Board board = boardService.read(5L);
+        System.out.println(board);
+    }
+
+    @Test
+    public void 게시물_수정_테스트() {
+        Board board = boardService.modify(4L, new BoardRequest(
+                "게시글변경", "테스트", "잘되나"));
+    }
+
+    public void 게시물_삭제_테스트 () {
+        boardService.remove(3L);
+        boardService.read(3L);
+    }
+}
