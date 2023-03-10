0628af844cc47660bfc74ba39aa1a30b1b957782
Hyeonvv <pphhww5017@gmail.com>
2023-03-10 21:31:56 +0900

    docs: 주석 추가

--- ParkHyeonwoo/spring/src/main/java/com/example/demo/domain/board/controller/request/BoardRequest.java
+++ ParkHyeonwoo/spring/src/main/java/com/example/demo/domain/board/controller/request/BoardRequest.java
@@ -3,7 +3,7 @@ package com.example.demo.domain.board.controller.request;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-
+// front 에서 전달받는것들
 @Getter
 @RequiredArgsConstructor
 public class BoardRequest {
--- ParkHyeonwoo/spring/src/main/java/com/example/demo/domain/board/entity/Board.java
+++ ParkHyeonwoo/spring/src/main/java/com/example/demo/domain/board/entity/Board.java
@@ -7,28 +7,29 @@ import org.hibernate.annotations.UpdateTimestamp;
 import javax.persistence.*;
 import java.util.Date;
 
-
-@Data
+@Data // getter, setter 자동 생성
 @Entity
 // class 이름으로 DB 가 생성된다.
 public class Board {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // 직접 할당 -> @Id 만 사용
+    // 자동 생성 -> @Id 와 @GeneratedValue 를 같이 사용
+    @Id // Primarykey(기본키) 로 설정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 새로운 PrimaryKey 를 생성해준다
     private Long boardId;
 
-    @Column(length = 128, nullable = false)
+    @Column(length = 128, nullable = false) // 문자 길이(String 타입에만 사용), default로 null 값의 허용 여부 결정
     private String title;
 
     @Column(length = 32, nullable = false)
     private String writer;
 
-    @Lob
+    @Lob // Large Object 의 줄임말, 대형 객체를 할당할 때 사용
     private String content;
 
-    @CreationTimestamp
+    @CreationTimestamp // INSERT 쿼리를 날릴 때 현재 시간 할당
     private Date regDate;
 
-    @UpdateTimestamp
+    @UpdateTimestamp // UPDATE 쿼리 날릴 때 현재 시간 할당
     private Date updDate;
 }
