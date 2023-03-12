303e54d37ec20403772e762723ec9e01239c3e39
vceqiv09 <vceqiv@gmail.com>
2023-03-12 22:23:57 +0900

    feat: [윤영현] 파일(이미지) 업로드 기능 추가

---
+++ YunYoungHyun/Spring/demo/demo/src/main/java/com/example/demo/domain/fileTest/controller/request/RequestFileInfo.java
@@ -0,0 +1,13 @@
+package com.example.demo.domain.fileTest.controller.request;
+
+import lombok.*;
+
+@Getter
+@ToString
+@RequiredArgsConstructor
+public class RequestFileInfo {
+
+     private Integer price;
+     private String test;
+
+}
