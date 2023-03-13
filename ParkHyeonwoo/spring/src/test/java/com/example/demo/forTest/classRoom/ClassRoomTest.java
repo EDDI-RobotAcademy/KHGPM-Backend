a7538612e2d73515de9c76dea43379e81b87e4e1
Hyeonvv <pphhww5017@gmail.com>
2023-03-13 20:25:39 +0900

    feat: 학생 조회, 반 수정 테스트

--- ParkHyeonwoo/spring/src/test/java/com/example/demo/forTest/classRoom/ClassRoomTest.java
+++ ParkHyeonwoo/spring/src/test/java/com/example/demo/forTest/classRoom/ClassRoomTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
 @SpringBootTest
 public class ClassRoomTest {
 
@@ -19,12 +23,44 @@ public class ClassRoomTest {
 
     @Test
     public void 학생_정보_저장() {
-        ClassRoom classRoom = new ClassRoom("첫번째 클래스");
+        ClassRoom classRoom = new ClassRoom("여긴어디반");
         classRoomRepository.save(classRoom);
 
-        TestStudent testStudent = new TestStudent("하이");
+        TestStudent testStudent = new TestStudent("어디");
 
         testStudent.setClassRoom(classRoom);
+        studentRepository.save(testStudent);
+    }
+
+    @Test
+    public void 학생_번호_조회() {
+        Optional<TestStudent> maybeStudent = studentRepository.findById(1L);
+        TestStudent testStudent = maybeStudent.get();
+
+        System.out.println(testStudent);
+    }
+
+    @Test
+    public void 학생_전체_조회() {
+        List<TestStudent> testStudentList = studentRepository.findAll();
+        System.out.println(testStudentList);
+    }
+
+    @Test
+    public void 반이름에_따른_학생_조회() {
+        List<TestStudent> studentList = studentRepository.findStudentListByRoomName("두번째 클래스");
+        System.out.println(studentList);
+    }
+
+    @Test
+    public void 반_변경() {
+        Optional<TestStudent> maybeStudent = studentRepository.findById(3L);
+        TestStudent testStudent = maybeStudent.get();
+
+        ClassRoom cr = new ClassRoom("새로운 반");
+        classRoomRepository.save(cr);
+
+        testStudent.setClassRoom(cr);
         studentRepository.save(testStudent);
     }
 }
