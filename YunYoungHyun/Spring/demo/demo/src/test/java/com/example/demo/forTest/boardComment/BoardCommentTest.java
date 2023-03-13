57bc796f6fe1480c56c88b78d9bb8b6793a1a97d
vceqiv09 <vceqiv@gmail.com>
2023-03-13 19:22:11 +0900

    test: [윤영현] 게시물 댓글 출력, 수정, 삭제, 게시글 삭제 테스트

--- YunYoungHyun/Spring/demo/demo/src/test/java/com/example/demo/forTest/boardComment/BoardCommentTest.java
+++ YunYoungHyun/Spring/demo/demo/src/test/java/com/example/demo/forTest/boardComment/BoardCommentTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.forTest.boardComment;
 
+import com.example.demo.domain.forTest.controller.response.CommentResponse;
 import com.example.demo.domain.forTest.entity.Comment;
 import com.example.demo.domain.forTest.entity.TestBoard;
 import com.example.demo.domain.forTest.repository.CommentRepository;
@@ -8,6 +9,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -34,6 +38,45 @@ public class BoardCommentTest {
 
         testBoardRepository.save(testBoard);
         commentRepository.save(comment);
+    }
+
+    @Test
+    public void 게시물_댓글_출력() {
+        List<Comment> commentList = commentRepository.findAllCommentsByBoardId(1L);
+        List<CommentResponse> commentResponses = new ArrayList<>();
+
+        for(Comment comment: commentList) {
+            commentResponses.add(new CommentResponse(comment.getContent()));
+        }
+
+        System.out.println("게시물 댓글 출력: "+ commentResponses);
+    }
+
+    @Test
+    public void 댓글_수정() {
+        Optional<Comment> maybeComment = commentRepository.findById(4L);
+        Comment comment = maybeComment.get();
+
+        comment.updateContent("이러쿵 저러쿵 요로쿵");
+        commentRepository.save(comment);
+    }
+
+    @Test
+    public void 댓글_삭제() {
+        commentRepository.deleteById(3L);
+    }
+
+    @Test
+    public void 게시글_삭제() {
+        final Long boardId = 3L;
+        List<Comment> commentList = commentRepository.findAllCommentsByBoardId(boardId);
+
+        for(Comment comment: commentList) {
+            System.out.println("게시글_삭제: "+ comment.getContent());
+            commentRepository.delete(comment);
+        }
+
+        testBoardRepository.deleteById(boardId);
     }
 
 }
