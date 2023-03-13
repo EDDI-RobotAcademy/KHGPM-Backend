c8c9981778346c5c3aa4d792e9e46fb5f619fb91
jmj30040 <jmj30040@gmail.com>
2023-03-13 16:56:09 +0900

    feat: [정명진] 게시물 덧글 달기 테스트 [BOARDx]

--- JeongMyeongjin/demo/src/test/java/com/example/demo/forTest/boardComment/BoardCommentTest.java
+++ JeongMyeongjin/demo/src/test/java/com/example/demo/forTest/boardComment/BoardCommentTest.java
@@ -1,19 +1,40 @@
 package com.example.demo.forTest.boardComment;
 
+import com.example.demo.domain.forTest.entity.Comment;
 import com.example.demo.domain.forTest.entity.TestBoard;
+import com.example.demo.domain.forTest.repository.CommentRepository;
 import com.example.demo.domain.forTest.repository.TestBoardRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 @SpringBootTest
 public class BoardCommentTest {
 
     @Autowired
     private TestBoardRepository testBoardRepository;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
     @Test
     public void 게시물_저장() {
         TestBoard board = new TestBoard("제목", "내용");
         testBoardRepository.save(board);
+    }
+
+    @Test
+    public void 덧글_저장() {
+        Optional<TestBoard> mayTeatBoard = testBoardRepository.findById(1L);
+        TestBoard testBoard = mayTeatBoard.get();
+
+        Comment comment = new Comment("덧글");
+
+        testBoard.setComment(comment);
+        testBoardRepository.save(testBoard);
+
+        commentRepository.save(comment);
     }
 }
