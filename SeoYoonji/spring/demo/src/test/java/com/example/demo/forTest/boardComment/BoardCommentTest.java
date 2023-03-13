d51fba7858d8fadb267700a95591bba6facd9be4
dduru-nuna <syj00062@gmail.com>
2023-03-13 17:14:28 +0900

    feat: [서윤지] 게시글 댓글 추가 & 출력

--- SeoYoonji/spring/demo/src/test/java/com/example/demo/forTest/boardComment/BoardCommentTest.java
+++ SeoYoonji/spring/demo/src/test/java/com/example/demo/forTest/boardComment/BoardCommentTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.forTest.boardComment;
 
+import com.example.demo.domain.forTest.controller.response.CommentResponse;
 import com.example.demo.domain.forTest.entity.Comment;
 import com.example.demo.domain.forTest.entity.TestBoard;
 import com.example.demo.domain.forTest.repository.CommentRepository;
@@ -8,6 +9,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -20,20 +24,32 @@ public class BoardCommentTest {
 
     @Test
     public void 게시물_저장() {
-        TestBoard board = new TestBoard("제목", "내용");
+        TestBoard board = new TestBoard("제목2", "내용2");
         testBoardRepository.save(board);
     }
 
     @Test
     public void 덧글_저장() {
-        Optional<TestBoard> mayTestBoard = testBoardRepository.findById(1L);
+        Optional<TestBoard> mayTestBoard = testBoardRepository.findById(2L);
         TestBoard testBoard = mayTestBoard.get();
 
-        Comment comment = new Comment("덧글");
+        Comment comment = new Comment("댓글추가");
 
         testBoard.setComment(comment);
         testBoardRepository.save(testBoard); //원래있던 게시글에 댓글을 추가한 것이니 '수정' 한거라 볼 수 있음
 
         commentRepository.save(comment);
+    }
+
+    @Test
+    public void 게시물_덧글_출력() {
+        List<Comment> commentList = commentRepository.findAllCommentsByBoardId(1L);
+        List<CommentResponse> commentResponses = new ArrayList<>();
+
+        for(Comment comment: commentList) {
+            commentResponses.add(new CommentResponse(comment.getContent()));
+        }
+        
+        System.out.println(commentResponses);
     }
 }
