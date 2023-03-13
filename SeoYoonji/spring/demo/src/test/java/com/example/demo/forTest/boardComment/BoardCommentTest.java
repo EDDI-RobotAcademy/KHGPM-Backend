ceaee7f1221bf67e28c0271aaec0165f4af1b0a2
dduru-nuna <syj00062@gmail.com>
2023-03-13 16:33:38 +0900

    feat: [서윤지] 저장 테스트

---
+++ SeoYoonji/spring/demo/src/test/java/com/example/demo/forTest/boardComment/BoardCommentTest.java
@@ -0,0 +1,19 @@
+package com.example.demo.forTest.boardComment;
+
+import com.example.demo.domain.forTest.entity.TestBoard;
+import com.example.demo.domain.forTest.repository.TestBoardRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BoardCommentTest {
+    @Autowired
+    private TestBoardRepository testBoardRepository;
+
+    @Test
+    public void 게시물_저장() {
+        TestBoard board = new TestBoard("제목", "내용");
+        testBoardRepository.save(board);
+    }
+}
