6a29b06977d45cb6336316cff859654f7b03a593
vceqiv09 <vceqiv@gmail.com>
2023-03-13 20:31:41 +0900

    test [윤영현] Student, ClassRoom 저장, 조회, 수정, 삭제 테스트 [JOIN TABLE 예제]

--- YunYoungHyun/Spring/demo/demo/src/main/java/com/example/demo/domain/forTest/student/repository/ClassRoomRepository.java
+++ YunYoungHyun/Spring/demo/demo/src/main/java/com/example/demo/domain/forTest/student/repository/ClassRoomRepository.java
@@ -3,7 +3,10 @@ package com.example.demo.domain.forTest.student.repository;
 import com.example.demo.domain.forTest.student.entity.ClassRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClassRoomRepository extends JpaRepository<ClassRoom, Long> {
 
+    Optional<ClassRoom> findByRoomName(String roomName);
 
 }
