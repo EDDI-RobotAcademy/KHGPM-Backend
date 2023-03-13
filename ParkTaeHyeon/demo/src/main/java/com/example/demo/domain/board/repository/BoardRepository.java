13ced17f5627f0e7c428e23f0d2027622be18474
taylor930422 <125858598+taylor930422@users.noreply.github.com>
2023-03-13 21:28:41 +0900

    feat: [박태현] register Domain 문제점 해결

--- ParkTaeHyeon/demo/src/main/java/com/example/demo/domain/board/repository/BoardRepository.java
+++ ParkTaeHyeon/demo/src/main/java/com/example/demo/domain/board/repository/BoardRepository.java
@@ -3,5 +3,11 @@ package com.example.demo.domain.board.repository;
 import com.example.demo.domain.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    Long countBy();
+
+    Board findFirstByOrderByBoardIdDesc();
 }
--- ParkTaeHyeon/demo/src/main/java/com/example/demo/domain/board/service/BoardService.java
+++ ParkTaeHyeon/demo/src/main/java/com/example/demo/domain/board/service/BoardService.java
@@ -6,7 +6,8 @@ import com.example.demo.domain.board.entity.Board;
 import java.util.List;
 
 public interface BoardService {
-    public void register(BoardRequest boardRequest);
+//    public void register(BoardRequest boardRequest);
+    public Board register(BoardRequest boardRequest);
 
     List<Board> list();
     Board read(Long boardId);
@@ -14,4 +15,8 @@ public interface BoardService {
     void remove(Long boardId);
 
     Board modify(Long boardId, BoardRequest boardRequest);
+
+    Long getCount();
+
+    Long getLastEntityId();
 }
