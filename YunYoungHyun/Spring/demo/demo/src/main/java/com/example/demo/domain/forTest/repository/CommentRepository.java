4488205b8f34efb25169d45dc260ee2f7d9a3f0c
vceqiv09 <vceqiv@gmail.com>
2023-03-13 19:20:37 +0900

    feat: [윤영현] boardId 에 맞는 댓글 가져오는 기능 추가

--- YunYoungHyun/Spring/demo/demo/src/main/java/com/example/demo/domain/forTest/repository/CommentRepository.java
+++ YunYoungHyun/Spring/demo/demo/src/main/java/com/example/demo/domain/forTest/repository/CommentRepository.java
@@ -2,7 +2,14 @@ package com.example.demo.domain.forTest.repository;
 
 import com.example.demo.domain.forTest.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query("select c from Comment c join c.testBoard tb where tb.id = :boardId")
+    List<Comment> findAllCommentsByBoardId(@Param("boardId") Long boardId);
 
 }
