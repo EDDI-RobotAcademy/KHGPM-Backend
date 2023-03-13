e76c41e3f24c200a4524a203b2c054a6dd139e70
dduru-nuna <syj00062@gmail.com>
2023-03-13 20:12:02 +0900

    feat: [서윤지] 조건, 전체 조회

--- SeoYoonji/spring/demo/src/test/java/com/example/demo/forTest/classRoom/ClassRoomTest.java
+++ SeoYoonji/spring/demo/src/test/java/com/example/demo/forTest/classRoom/ClassRoomTest.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+import java.util.Optional;
+
 @SpringBootTest
 public class ClassRoomTest {
 
@@ -19,13 +22,32 @@ public class ClassRoomTest {
 
     @Test
     public void 학생_정보_저장() {
-        ClassRoom classRoom = new ClassRoom("첫번째 클래스");
+        ClassRoom classRoom = new ClassRoom("두번째 클래스");
         classRoomRepository.save(classRoom);
 
-        TestStudent testStudent = new TestStudent("하이");
+        TestStudent testStudent = new TestStudent("뚤뚤");
 
         testStudent.setClassRoom(classRoom);
         studentRepository.save(testStudent);
     }
 
+    @Test
+    public void 학생_번호_조회() {
+        Optional<TestStudent> maybeStudent = studentRepository.findById(1L);
+        TestStudent testStudent = maybeStudent.get();
+
+        System.out.println(testStudent);
+    }
+
+    @Test
+    public void 학생_전체_조회() {
+        List<TestStudent> testStudentList = studentRepository.findAll();
+        System.out.println(testStudentList);
+    }
+
+    @Test
+    public void 각_반_학생_조회() {
+        List<TestStudent> studentList = studentRepository.findStudentListByRoomName("두번째 클래스");
+        System.out.println(studentList);
+    }
 }
