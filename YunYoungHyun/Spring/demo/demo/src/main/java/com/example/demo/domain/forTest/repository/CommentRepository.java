d7cf0d9c43a1697ee73b8020178cde227f906826
vceqiv09 <vceqiv@gmail.com>
2023-03-13 16:36:02 +0900

    feat: [윤영현] 게시물 댓글 기능 Entity, Repository 추가

---
+++ YunYoungHyun/Spring/demo/demo/src/main/java/com/example/demo/domain/forTest/repository/CommentRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.domain.forTest.repository;
+
+import com.example.demo.domain.forTest.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+}
