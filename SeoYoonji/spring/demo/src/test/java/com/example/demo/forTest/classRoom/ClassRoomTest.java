f339e5ddeb2612155d980cc60303612229c5fd40
dduru-nuna <syj00062@gmail.com>
2023-03-13 20:28:43 +0900

    feat: [서윤지] 반 변경 & 학생 삭제

--- SeoYoonji/spring/demo/src/test/java/com/example/demo/forTest/classRoom/ClassRoomTest.java
+++ SeoYoonji/spring/demo/src/test/java/com/example/demo/forTest/classRoom/ClassRoomTest.java
@@ -22,7 +22,7 @@ public class ClassRoomTest {
 
     @Test
     public void 학생_정보_저장() {
-        ClassRoom classRoom = new ClassRoom("두번째 클래스");
+        ClassRoom classRoom = new ClassRoom("블라블라클래스");
         classRoomRepository.save(classRoom);
 
         TestStudent testStudent = new TestStudent("뚤뚤");
@@ -49,5 +49,26 @@ public class ClassRoomTest {
     public void 각_반_학생_조회() {
         List<TestStudent> studentList = studentRepository.findStudentListByRoomName("두번째 클래스");
         System.out.println(studentList);
+    }
+
+    @Test
+    public void 반_변경() {
+        Optional<TestStudent> maybeStudent = studentRepository.findById(5L);
+        TestStudent testStudent = maybeStudent.get();
+
+        ClassRoom cr = new ClassRoom("새로운 반");
+        classRoomRepository.save(cr);
+
+        testStudent.setClassRoom(cr);
+        studentRepository.save(testStudent);
+    }
+
+    @Test
+    public void 삭제() {
+        Optional<TestStudent> maybeStudent = studentRepository.findById(4L);
+        TestStudent testStudent = maybeStudent.get();
+
+        testStudent.setClassRoom(null);
+        studentRepository.delete(testStudent);
     }
 }
