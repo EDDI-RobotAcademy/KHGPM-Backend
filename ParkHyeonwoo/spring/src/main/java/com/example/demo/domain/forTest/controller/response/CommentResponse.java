5721c3ab121d6f83b355de091e0add6d06a31a5f
Hyeonvv <pphhww5017@gmail.com>
2023-03-13 17:30:29 +0900

    feat: 게시물 댓글 출력 테스트

---
+++ ParkHyeonwoo/spring/src/main/java/com/example/demo/domain/forTest/controller/response/CommentResponse.java
@@ -0,0 +1,7 @@
+package com.example.demo.domain.forTest.controller.response;
+
+public class CommentResponse {
+    private String comment;
+
+    public CommentResponse (String comment) { this.comment = comment; }
+}
--- ParkHyeonwoo/spring/src/main/java/com/example/demo/domain/forTest/repository/CommentRepository.java
+++ ParkHyeonwoo/spring/src/main/java/com/example/demo/domain/forTest/repository/CommentRepository.java
@@ -2,6 +2,12 @@ package com.example.demo.domain.forTest.repository;
 
 import com.example.demo.domain.forTest.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query("select c from Comment c join c.testBoard tb where tb.id = :boardId") // 쿼리문 직접 사용
+    List<Comment> findAllCommentsByBoardId(Long boardId);    // boardId = ↑
 }
