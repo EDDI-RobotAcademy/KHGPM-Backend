6bc1f70719a594508a31a8d93b25e374cd7b4a68
Sanghoon Lee <gcccompil3r@gmail.com>
2023-03-14 19:54:39 +0900

    feat: 이미지 기반 상품 등록 완료 [PRODUCT-IMAGEx]

--- LeeSanghoon/src/main/java/com/example/demo/domain/product/service/ProductServiceImpl.java
+++ LeeSanghoon/src/main/java/com/example/demo/domain/product/service/ProductServiceImpl.java
@@ -1,13 +1,20 @@
 package com.example.demo.domain.product.service;
 
-import com.example.demo.domain.board.entity.Board;
-import com.example.demo.domain.product.controller.request.ProductRequest;
+import com.example.demo.domain.product.controller.dto.ProductRequest;
+import com.example.demo.domain.product.controller.dto.RequestProductInfo;
+import com.example.demo.domain.product.entity.ImageResource;
 import com.example.demo.domain.product.entity.Product;
+import com.example.demo.domain.product.repository.ImageResourceRepository;
 import com.example.demo.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,9 +24,16 @@ import java.util.Optional;
 public class ProductServiceImpl implements ProductService {
 
     final private ProductRepository productRepository;
+    final private ImageResourceRepository imageResourceRepository;
 
     @Override
-    public void register(ProductRequest productRequest) {
+    public void register(List<MultipartFile> fileList, RequestProductInfo productRequest) {
+        log.info("글자 출력: " + productRequest);
+
+        List<ImageResource> imageResourceList = new ArrayList<>();
+
+        final String fixedStringPath = "../../KHGPM-Frontend/LeeSanghoon/frontend/src/assets/uploadImgs/";
+
         Product product = new Product();
 
         product.setProductName(productRequest.getProductName());
@@ -27,7 +41,34 @@ public class ProductServiceImpl implements ProductService {
         product.setContent(productRequest.getContent());
         product.setPrice(productRequest.getPrice());
 
+        try {
+            for (MultipartFile multipartFile : fileList) {
+                log.info("requestFileUploadWithText() - filename: " + multipartFile.getOriginalFilename());
+
+                String fullPath = fixedStringPath + multipartFile.getOriginalFilename();
+
+                FileOutputStream writer = new FileOutputStream(
+                        fixedStringPath + multipartFile.getOriginalFilename()
+                );
+
+                writer.write(multipartFile.getBytes());
+                writer.close();
+
+                ImageResource imageResource = new ImageResource(fullPath);
+                imageResourceList.add(imageResource);
+                product.setImageResource(imageResource);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         productRepository.save(product);
+
+        for (ImageResource imageResource: imageResourceList) {
+            imageResourceRepository.save(imageResource);
+        }
     }
 
     @Override
