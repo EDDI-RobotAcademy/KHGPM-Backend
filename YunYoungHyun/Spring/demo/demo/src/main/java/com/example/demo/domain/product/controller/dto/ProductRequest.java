39b758fea5ba3617d3cbd8778750b1b256380990
vceqiv09 <vceqiv@gmail.com>
2023-03-15 19:12:05 +0900

    rename: [윤영현] ProductRequest.java dto 패키지로 이동

---
+++ YunYoungHyun/Spring/demo/demo/src/main/java/com/example/demo/domain/product/controller/dto/ProductRequest.java
@@ -0,0 +1,16 @@
+package com.example.demo.domain.product.controller.dto;
+
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.Lob;
+
+@Getter
+public class ProductRequest {
+
+    private String name;
+    private Long price;
+    private String writer;
+    private String content;
+
+}
