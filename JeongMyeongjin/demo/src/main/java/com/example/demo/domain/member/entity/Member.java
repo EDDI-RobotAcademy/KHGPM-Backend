4a990a4f61433b74a8fadb44795371065afc9ef8
jmj30040 <jmj30040@gmail.com>
2023-03-17 00:24:32 +0900

    feat: [정명진] 회원가입, 로그인 일부 구현 및 가입되지 않은 사용자 로그인 테스트 [MEMBERx]

--- JeongMyeongjin/demo/src/main/java/com/example/demo/domain/member/entity/Member.java
+++ JeongMyeongjin/demo/src/main/java/com/example/demo/domain/member/entity/Member.java
@@ -4,6 +4,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 @Entity
 @NoArgsConstructor
@@ -17,4 +20,34 @@ public class Member {
     @Getter
     @Column(nullable = false)
     private String email;
+
+    @OneToOne(mappedBy = "member", fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    private MemberProfile memberProfile;
+
+    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
+    private Set<Authentication> authentications = new HashSet<>();
+
+    public Member(String email, MemberProfile memberProfile) {
+        this.email = email;
+        this.memberProfile = memberProfile;
+        memberProfile.setMember(this);
+    }
+
+    public boolean isRightPassword(String plainToCheck) {
+        final Optional<Authentication> maybeBasicAuth = findBasicAuthentication();
+
+        if (maybeBasicAuth.isPresent()) {
+            final BasicAuthentication authentication = (BasicAuthentication) maybeBasicAuth.get();
+            return authentication.isRightPassword(plainToCheck);
+        }
+
+        return false;
+    }
+
+    private Optional<Authentication> findBasicAuthentication () {
+        return authentications
+                .stream()
+                .filter(authentication -> authentication instanceof BasicAuthentication)
+                .findFirst();
+    }
 }
