8fda4616ca4eed0f92ec4e1fb189061f963e992d
Sanghoon Lee <gcccompil3r@gmail.com>
2023-03-24 21:16:49 +0900

    fix: Put & Delete Mapping Cors Process [CORSx]

--- LeeSanghoon/src/main/java/com/example/demo/domain/config/WebConfig.java
+++ LeeSanghoon/src/main/java/com/example/demo/domain/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:8080")
-                .allowedOrigins("http://localhost:8080");
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
