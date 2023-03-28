85a169c21a3900b0c3482061f656415a61448262
Sanghoon Lee <gcccompil3r@gmail.com>
2023-03-28 18:10:55 +0900

    feat: [이상훈] 일반 게시판 테스트 완료 [BOARD-TESTx]

--- LeeSanghoon/src/test/java/com/example/demo/nonRepoBoardTest/NonRepoBoardTest.java
+++ LeeSanghoon/src/test/java/com/example/demo/nonRepoBoardTest/NonRepoBoardTest.java
@@ -20,15 +20,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @DisplayName("일반 게시판에 대한 테스트")
 @SpringBootTest
@@ -107,6 +105,19 @@ public class NonRepoBoardTest {
 
         assertThat(mockBoardRepository.findById(0L).get()).isEqualTo(actual);
     }
-    
 
+    @DisplayName("Delete method로 게시물 삭제 테스트")
+    @Test
+    public void 게시물_삭제_테스트 () {
+        Board board = new Board("제목", "작성자", "내용");
+        Optional<Board> maybeBoard = Optional.of(board);
+
+        when(mockBoardRepository.findById(0L))
+                .thenReturn(maybeBoard);
+
+        final BoardServiceImpl sut = new BoardServiceImpl(mockBoardRepository);
+        sut.remove(0L);
+
+        verify(mockBoardRepository, times(1)).deleteById(0L);
+    }
 }
